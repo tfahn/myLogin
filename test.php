03a599c6da2a9a3a80432a0abd3e75966c734dd5
Tristan <trispammail@web.de>
2017-05-10 21:29:19 +0200

    First Commit

--- test.php
+++ test.php
@@ -1,1 +1,32 @@
-abcdefghijklmnopqrstuvwxyz
+<?php
+if(!(isset($_SESSION['eingeloggt'])) && !($_SESSION['eingeloggt'] == 1)) {
+    if (isset($_POST['Login'])) {
+        if(($_POST['username'] == 'Username') && ($_POST['pass'] == 'Passwort')) {
+            $_SESSION['eingeloggt'] = 1;
+        }else{
+            echo("Falsche Zugangsdaten");
+            echo("<br><a href='test.php'>Noch einmal versuchen</a> ");
+        }
+    } else {
+        ?>
+
+        <form action="test.php" method="post">
+            Benutzername:<input type="text" name="username"><br>
+            Passwort:<input type="password" name="pass"><br>
+            <input type="submit" name="Login">
+        </form>
+
+        <?php
+    }
+}else{
+    if(isset($_POST['Logout'])){
+        session_destroy();
+    }else{
+        ?>
+        <form action="test.php" method="post">
+            <input type="submit" name="Logout">
+        </form>
+<?php
+    }
+}
+?>
