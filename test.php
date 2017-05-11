580e65c1da15b424d26812ce1d94cf8a6573fc3b
Manuel Huber <ManuelHu@users.noreply.github.com>
2017-05-11 17:40:06 +0200

    Code aufgeräumt
    Die erste if-Bedinung habe ich angepasst, die Logik stimmte nicht (Verknüpfung mit && statt ||)

--- test.php
+++ test.php
@@ -1,32 +1,43 @@
 <?php
-if(!(isset($_SESSION['eingeloggt'])) && !($_SESSION['eingeloggt'] == 1)) {
-    if (isset($_POST['Login'])) {
-        if(($_POST['username'] == 'Username') && ($_POST['pass'] == 'Passwort')) {
+session_start();
+
+function check_password( $user, $pw ) {
+	//TODO: Aus Datenbank & gehasht
+	return 'user' == $user && 'pw' == $pw;
+}
+
+if ( ! isset( $_SESSION['eingeloggt'] ) || $_SESSION['eingeloggt'] != 1 ) {
+    if ( isset( $_POST['login'] ) ) {
+		
+		$username = $_POST['username'];
+		$password = $_POST['pass'];
+		
+        if ( check_password( $username, $password ) ) {
+			// Login erfolgreich
             $_SESSION['eingeloggt'] = 1;
-        }else{
+        } else {
             echo("Falsche Zugangsdaten");
-            echo("<br><a href='test.php'>Noch einmal versuchen</a> ");
+            echo("<br><a href='login.php'>Noch einmal versuchen</a> ");
         }
     } else {
         ?>
 
-        <form action="test.php" method="post">
+        <form action="login.php" method="post">
             Benutzername:<input type="text" name="username"><br>
             Passwort:<input type="password" name="pass"><br>
-            <input type="submit" name="Login">
+            <input type="submit" name="login">
         </form>
 
         <?php
     }
-}else{
-    if(isset($_POST['Logout'])){
+} else {
+    if( isset( $_POST['logout'] ) ) {
         session_destroy();
-    }else{
+    } else {
         ?>
-        <form action="test.php" method="post">
-            <input type="submit" name="Logout">
+        <form action="login.php" method="post">
+            <input type="submit" name="logout">
         </form>
 <?php
     }
 }
-?>
