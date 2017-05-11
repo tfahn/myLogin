4bfc8bd2be9ece7d0ab0b709f77b440f3bca5200
Manuel Huber <ManuelHu@users.noreply.github.com>
2017-05-11 17:55:57 +0200

    Eigene Methode zum Check des Eingeloggt

--- login.php
+++ login.php
@@ -6,20 +6,26 @@ function check_password( $user, $pw ) {
 	return 'user' == $user && 'pw' == $pw;
 }
 
+function is_logged_in() {
+	return isset( $_SESSION['logged-in'] ) && $_SESSION['logged-in'] == 1;
+}
+
+// Zuerst prüfen, damit beim Logout gleich das Formular wieder angezeigt wird
 if ( isset( $_GET['logout'] ) ) {
 	session_destroy();
 	unset ( $_SESSION['logged-in'] );
 }
 
-if ( ! isset( $_SESSION['logged-in'] ) || $_SESSION['logged-in'] != 1 ) {
+if ( ! is_logged_in() ) {
     if ( isset( $_POST['login'] ) ) {
 		
 		$username = $_POST['username'];
 		$password = $_POST['pass'];
 		
         if ( check_password( $username, $password ) ) {
-	    // Login erfolgreich
+			// Login erfolgreich
             $_SESSION['logged-in'] = 1;
+			$_SESSION['user'] = $username;
 			header('Location: login.php');
         } else {
             echo("Falsche Zugangsdaten");
@@ -31,14 +37,14 @@ if ( ! isset( $_SESSION['logged-in'] ) || $_SESSION['logged-in'] != 1 ) {
         <form action="login.php" method="post">
             Benutzername: <input type="text" name="username"><br>
             Passwort: <input type="password" name="pass"><br>
-            <input type="submit" name="login">
+            <input type="submit" name="login" value="Anmelden">
         </form>
 
         <?php
     }
 } else {
 ?>
-	<p>Hier steht was geheimes</p>	
+	<p>Hallo <?php echo $_SESSION['user']; ?>, hier steht was geheimes</p>	
 	<a href="login.php?logout=1">Ausloggen</a>
 <?php
 }
