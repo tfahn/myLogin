f40817f105e1adf1734a6a5363f867f06ecde967
Manuel Huber <ich@manuelhu.de>
2017-05-11 18:30:08 +0200

    Multiuser

--- login.php
+++ login.php
@@ -1,11 +1,17 @@
 <?php
 include_once( 'login-inc.php' );
 
-function check_password( $user, $pw ) {	
+function check_password( $user, $pw ) {
+	$users = array( 
+		'user' => '$2y$10$4b.HEihcOIOkaWSrr8P/UeSQsKvBGDqHBiuIB7AezgbATVqkZ/HLC', // PW: pw
+		'test' => '$2y$10$R26LvhVF4y68sak69p2qVeWjh1e.ybDV0WQ7HpgFhS8WhjfdeSzze', // PW: test
+	);
 	//TODO: Aus Datenbank
 	
-	$hash = "$2y$10$4b.HEihcOIOkaWSrr8P/UeSQsKvBGDqHBiuIB7AezgbATVqkZ/HLC";
-	return 'user' == $user && password_verify( $pw, $hash );
+	if ( !array_key_exists( $user, $users ) )
+		return false;
+	
+	return password_verify( $pw, $users[ $user ] );
 }
 
 // Zuerst prüfen, damit beim Logout gleich das Formular wieder angezeigt wird
